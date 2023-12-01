c4e627131dbdee5dbb994698f3dcd1bdf3c4b751
Catalin Cristu <catalin.cristu@gmail.com>
2023-12-01 14:35:23 +0200

    Add template for day problem solver

---
+++ day_tpl.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# Template for AoC day solver
+
+import regex as re
+from utils import get_day_input
+
+
+def part1(lines: list[str]):
+    pass
+
+
+def part2(lines: list[str]):
+    pass
+
+
+def main():
+    lines = get_day_input(1)
+    print(part1(lines))
+    print(part2(lines))
+
+
+if __name__ == "__main__":
+    main()
