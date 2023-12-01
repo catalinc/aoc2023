fb798e3d1c67613429971177864e43205635cad4
Catalin Cristu <catalin.cristu@gmail.com>
2023-12-01 14:40:52 +0200

    Add the option to use a test file

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-def get_day_input(day: int) -> list[str]:
-    with open(f"input/day{day}.txt",  encoding="utf-8") as infile:
+def get_day_input(day: int, is_test: bool = False) -> list[str]:
+    suffix = "" if not is_test else "_test"
+    with open(f"input/day{day}{suffix}.txt", encoding="utf-8") as infile:
         return [line.strip() for line in infile]
-
