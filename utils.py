c5343f1cbade49bf16c31c436d4f1f2d5f4f6c2e
Catalin Cristu <catalin.cristu@gmail.com>
2023-12-02 10:48:09 +0200

    Simplify the option to use a test file

--- utils.py
+++ utils.py
@@ -1,4 +1,8 @@
-def get_day_input(day: int, is_test: bool = False) -> list[str]:
+import sys
+
+
+def get_day_input(day: int) -> list[str]:
+    is_test = "-t" in sys.argv
     suffix = "" if not is_test else "_test"
     with open(f"input/day{day}{suffix}.txt", encoding="utf-8") as infile:
         return [line.strip() for line in infile]
