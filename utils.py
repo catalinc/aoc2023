5775dad5d2d0c7677ab3845799c3c7acdcd81ef3
Catalin Cristu <catalin.cristu@gmail.com>
2023-12-09 09:28:03 +0200

    Read input file to string

--- utils.py
+++ utils.py
@@ -6,3 +6,15 @@ def get_day_input(day: int) -> list[str]:
     suffix = "" if not is_test else "_test"
     with open(f"input/day{day}{suffix}.txt", encoding="utf-8") as infile:
         return [line.strip() for line in infile]
+
+
+def get_day_input_full_content(day: int) -> str:
+    is_test = "-t" in sys.argv
+    suffix = "" if not is_test else "_test"
+    with open(f"input/day{day}{suffix}.txt", encoding="utf-8") as infile:
+        return infile.read()
+
+
+def chunks(lst, size: int):
+    for i in range(0, len(lst), size):
+        yield lst[i:i + size]
